5076a53dd48c5844101fda35f9b0151454f110d7
vikasgodara290 <vikasgodara290@gmail.com>
2025-02-23 17:02:51 +0530

    create button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
+import { AddIcon } from "./components/icon/AddIcon"
+import { ShareIcon } from "./components/icon/ShareIcon"
 import { Button } from "./components/ui/Button"
 
 function App() {
 
   return (
     <>
-      <Button text="Add Content"/>
+      <Button text="Add Content" varient="primary" frontIcon={<AddIcon/>}/>
+      <Button text="Share Brain" varient="secondary" frontIcon={<ShareIcon/>}/>
     </>
   )
 }
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,15 +1,23 @@
 interface ButtonProps{
     text: string,
-    varient?: "primary" | "secondary",
+    varient: "primary" | "secondary",
     size?: "sm" | "lg" | "md",
     frontIcon?: any,
     onClick?: ()=> void
 }
 
+const buttonVarient = {
+    "primary" : "bg-purple-700 text-white hover:bg-purple-300 hover:text-purple-700",
+    "secondary": "bg-purple-300 text-purple-700 hover:bg-purple-700 hover:text-white"
+}
+const defaultButtonStyle = "w-38 h-10 font-light rounded-md flex items-center mx-2 my-2 justify-center cursor-pointer";
+
 export function Button(props: ButtonProps){
     return(
         <>
-            <button className=" bg-purple-700">{props.text}</button>
+            <button className={`${buttonVarient[props.varient]} ${defaultButtonStyle}`}>
+                {props.frontIcon} <span className="pr-1"></span>
+            {props.text}</button>
         </>
     )
 }
