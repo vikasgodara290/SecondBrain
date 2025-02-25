f0d74f4c3d42068596498e7d02f787852d5396ce
vikasgodara290 <vikasgodara290@gmail.com>
2025-02-25 23:27:33 +0530

    Create UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,42 @@
 import { AddIcon } from "./components/icon/AddIcon"
+import { DocumentIcon } from "./components/icon/DocumentIcon"
 import { ShareIcon } from "./components/icon/ShareIcon"
 import { Button } from "./components/ui/Button"
+import { ItemCard } from "./components/ui/ItemCard"
 import { SideBar } from "./components/ui/SideBar"
 
 function App() {
 
   return (
     <>
-      {/* <Button text="Add Content" varient="primary" frontIcon={<AddIcon/>}/>
-      <Button text="Share Brain" varient="secondary" frontIcon={<ShareIcon/>}/> */}
-      {/* <SideBar/> */}
+      
+      <div className="">
+        <div>
+          <SideBar/>
+        </div>
+
+        <div className="flex justify-end p-4 items-center">
+          <span className="text-2xl flex-none ml-110 font-semibold">All Notes</span>
+          <div className="flex justify-end items-center flex-1">
+            <Button text="Add Content" varient="primary" frontIcon={<AddIcon/>}/>
+            <Button text="Share Brain" varient="secondary" frontIcon={<ShareIcon/>}/>
+          </div>
+        </div>
+      </div>
+
+      
+      <div className="ml-110 grid grid-cols-4 gap-10 mt-10">
+          <ItemCard icon={<DocumentIcon/>} title="Project Ideas" date={`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`}
+          content="there are some ideas about project : 1. T3 chat 2. Think new"/>
+          <ItemCard icon={<DocumentIcon/>} title="Project Ideas" date={`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`}
+          content="there are some ideas about project : 1. T3 chat 2. Think new"/>
+          <ItemCard icon={<DocumentIcon/>} title="Project Ideas" date={`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`}
+          content="there are some ideas about project : 1. T3 chat 2. Think new"/>
+          <ItemCard icon={<DocumentIcon/>} title="Project Ideas" date={`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`}
+          content="there are some ideas about project : 1. T3 chat 2. Think new"/>
+          <ItemCard icon={<DocumentIcon/>} title="Project Ideas" date={`${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`}
+          content="there are some ideas about project : 1. T3 chat 2. Think new"/>
+      </div>
       
     </>
   )
--- frontend/src/components/ui/ItemCard.tsx
+++ frontend/src/components/ui/ItemCard.tsx
@@ -1,29 +1,31 @@
 import { ReactElement } from "react"
+import { DeleteIcon } from "../icon/DeleteIcon"
+import { ShareIcon } from "../icon/ShareIcon"
 
 interface ItemCardProps{
     icon: ReactElement,
     title: string,
     content: string,
-    tags: Array<ReactElement>
-    date: Date
+    tags?: Array<ReactElement>
+    date: string
 }
 
 
-export function ItemCard(){
+export function ItemCard({icon, title, content, tags, date}: ItemCardProps){
     return(
-        <div>
-            <div>
-                <span>i</span>
-                <span>Project Ideas</span>
-                <span>s</span>
-                <span>d</span>
+        <div className="border w-70 h-80 rounded-md border-purple-300 shadow">
+            <div className="flex justify-around items-center w-full h-1/6">
+                <span>{icon}</span>
+                <span>{title}</span>
+                <span><DeleteIcon/></span>
+                <span><ShareIcon/></span>
             </div>
-            <div>Content</div>
-            <div>
-                <span>#productivity</span>
-                <span>#ideas</span>
+            <div className="text-2xl w-full h-1/2 px-2">{content}</div>
+            <div className="flex w-full h-1/6">
+                <span className="bg-purple-300 p-1.5 px-3 rounded-full text-sm h-1/2 flex items-center ml-2">#productivity</span>
+                <span className="bg-purple-300 p-1.5 px-3 rounded-full text-sm h-1/2 flex items-center ml-2">#ideas</span>
             </div>
-            <div>Added on 10/03/2024</div>
+            <div className="text-gray-500 w-full h-1/6 flex items-center ml-2">Added on: {date}</div>
         </div>
     )
 }
